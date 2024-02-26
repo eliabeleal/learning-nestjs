b767b73716578af7c126ae7febd316072d9ed88f
Eliabe Leal <elyabeleal@yahoo.com.br>
2024-02-26 18:35:15 -0300

    finalizando o projeto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,85 @@
-import {styled} from '../styles'
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
 
-const Button = styled('button', {
-  backgroundColor: '$rocketseat'
-})
+import { useKeenSlider } from "keen-slider/react";
 
-export default function Home() {
+import "keen-slider/keen-slider.min.css";
+import { stripe } from "../lib/stripe";
+import Stripe from "stripe";
+import Link from "next/link";
+
+interface HomeProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+  }[];
+}
+
+export default function Home({ products }: HomeProps) {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <Button>Enviar</Button>
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        {products.map((product) => {
+          return (
+            <Link
+              key={product.id}
+              href={`/product/${product.id}`}
+              prefetch={false}
+            >
+              <Product className="keen-slider__slide">
+                <Image src={product.imageUrl} width={520} height={480} alt="" />
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>
+          );
+        })}
+      </HomeContainer>
+    </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  // PÁGINAS ESTÁTICAS SÃO PÁGINAS QUE TODOS OS USUÁRIOS TEM ACESSO, POR ISSO NÃO TEM CONTEXTO, NEM RES E NEM REQ
+  // SE A PÁGINA PRECISA DE ALGUMA INFORMAÇÃO DE CONTEXTO, LOGO NÃO DÁ PRA SER UMA PÁGINA ESTÁTICA
+  const response = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
+
+  const products = response.data.map((product) => {
+    const { unit_amount } = product.default_price as Stripe.Price;
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(unit_amount ? unit_amount / 100 : 0),
+    };
+  });
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 60 * 60 * 2,
+  };
+};
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,129 @@
-import { useRouter } from "next/router"
+import { stripe } from "@/src/lib/stripe";
+import {
+  ImagemContainer,
+  ProductContainer,
+  ProductDetails,
+} from "@/src/styles/pages/product";
+import axios from "axios";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import Stripe from "stripe";
 
-export default function Product(){
-    const {query} = useRouter();
-    return (
-        <h1>Products {JSON.stringify(query)}</h1>
-    )
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+    defaultPriceId: string;
+  };
 }
+
+export default function Product({ product }: ProductProps) {
+  const [isCreatingCheckuotSession, setIsCreatingCheckuotSession] =
+    useState(false);
+
+  // const router = useRouter();
+
+  async function handleBuyProduct() {
+    try {
+      setIsCreatingCheckuotSession(true);
+      const response = await axios.post("/api/checkuot", {
+        priceId: product.defaultPriceId,
+      });
+
+      const { checkuotUrl } = response.data;
+
+      // PARA REDIRECIONAR PARA UMA ROTA INTERNA DO NEXT
+      // router.push("/checkuot");
+
+      // PARA REDIRECIONAR PARA UMA ROTA EXTERNA A APLICAÇÃO
+
+      window.location.href = checkuotUrl;
+    } catch (err) {
+      // CONECTAR COM UMA FERRAMENTA DE OBSERVABILIDADE (DATADOG/ SENTRY)
+      setIsCreatingCheckuotSession(false);
+
+      alert("Falha ao redirecionar ao checkuot!");
+    }
+  }
+
+  const { isFallback } = useRouter();
+
+  if (isFallback) {
+    return <p>Loding...</p>;
+  }
+
+  return (
+    <>
+      <Head>
+        <title>{product.name} | Ignite Shop</title>
+      </Head>
+      <ProductContainer>
+        <ImagemContainer>
+          <Image src={product.imageUrl} width={520} height={480} alt="" />
+        </ImagemContainer>
+        <ProductDetails>
+          <h1>{product.name}</h1>
+          <span>{product.price}</span>
+
+          <p>{product.description}</p>
+
+          <button
+            disabled={isCreatingCheckuotSession}
+            onClick={handleBuyProduct}
+          >
+            Comprar agora
+          </button>
+        </ProductDetails>
+      </ProductContainer>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const productId = params?.id ?? "";
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ["default_price"],
+  });
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        }).format(price.unit_amount ? price.unit_amount / 100 : 0),
+        description: product.description,
+        defaultPriceId: price.id,
+      },
+    },
+    revalidate: 60 * 60 * 1,
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  // BUSCAR OS PRODUTOS MAIS ACESSADOS DO ECOMERCE
+  return {
+    paths: [
+      {
+        params: {
+          id: "prod_Pcdjr5ND3sgXNU",
+        },
+      },
+    ],
+    fallback: true,
+  };
+};
